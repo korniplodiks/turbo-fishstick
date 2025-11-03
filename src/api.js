368b77aa92020829726508fc8116e317e6e02faf
korniplodiks <korniplodiks@users.noreply.github.com>
2025-11-03 10:19:14 +0200

    test(db): update configuration defaults

--- src/api.js
+++ src/api.js
@@ -50,3 +50,30 @@ class API {
 
 // Update 23
 module.exports = API;
+
+
+// API module for FishstickAPI
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 25
+module.exports = API;
